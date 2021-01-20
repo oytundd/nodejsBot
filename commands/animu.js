c0d137a7e2c5c2e7e5da8f81aa8e1fbf4fc78f5b
oytundd <oytundd@gmail.com>
2021-01-20 07:50:13 +0300

    .animu added
    get a random image from https://thisanimedoesnotexist.ai/

---
+++ commands/animu.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+
+module.exports = {
+  name: 'animu',
+  execute(message){
+    randInt = Math.floor(Math.random()*9999)+1;
+    url = "https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png";
+    message.channel.send(url);
+   }
+};
