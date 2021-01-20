896526b7578d026039b7956ad003296e35366563
oytundd <oytundd@gmail.com>
2021-01-20 11:52:18 +0300

    Update animu.js
    Images are now spoilered due to some images being nsfw

--- commands/animu.js
+++ commands/animu.js
@@ -3,13 +3,14 @@ const Discord = require('discord.js');
 
 module.exports = {
   name: 'animu',
+  description: 'Get a random ai generated anime picture from thisanimedoesnotexist.ai, enter a value between 0.3 and 2 for creativity value, default is 2',
   execute(message,args){
     randInt = Math.floor(Math.random()*9999)+1000;
     if(args[0] != undefined){
       if(args[0] >= 0.3 &&  args[0] <= 2){
         let num = args[0];
         num = Number(num).toFixed(1);
-        let url = "https://thisanimedoesnotexist.ai/results/psi-"+num+"/seed"+randInt+".png";
+        let url = "||https://thisanimedoesnotexist.ai/results/psi-"+num+"/seed"+randInt+".png||";
         message.channel.send(url);
       }
       else{
@@ -17,7 +18,7 @@ module.exports = {
       }
     }
     else{
-      let url = "https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png";
+      let url = "||https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png||";
       message.channel.send(url);
     }
 
