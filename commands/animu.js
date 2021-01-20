c12f1f84fee39e5f344948d84c79ef1211185a1d
oytundd <oytundd@gmail.com>
2021-01-20 07:52:40 +0300

    Update animu.js
    fixed correct random number

--- commands/animu.js
+++ commands/animu.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'animu',
   execute(message){
-    randInt = Math.floor(Math.random()*9999)+1;
+    randInt = Math.floor(Math.random()*9999)+1000;
     url = "https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png";
     message.channel.send(url);
    }
