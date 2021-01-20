544bf8230ebdc2ad8a5c6efd313f96056a8dff74
oytundd <oytundd@gmail.com>
2021-01-20 11:36:57 +0300

    .animu update
    Optional argument to customize creativity levels.

--- commands/animu.js
+++ commands/animu.js
@@ -3,9 +3,26 @@ const Discord = require('discord.js');
 
 module.exports = {
   name: 'animu',
-  execute(message){
+  execute(message,args){
     randInt = Math.floor(Math.random()*9999)+1000;
-    url = "https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png";
-    message.channel.send(url);
+    if(args[0] != undefined){
+      if(args[0] >= 0.3 &&  args[0] <= 2){
+        let num = args[0];
+        num = Number(num).toFixed(1);
+        let url = "https://thisanimedoesnotexist.ai/results/psi-"+num+"/seed"+randInt+".png";
+        message.channel.send(url);
+      }
+      else{
+        message.channel.send("Please enter a value between 0.3 and 2 for creativity value.");
+      }
+    }
+    else{
+      let url = "https://thisanimedoesnotexist.ai/results/psi-2.0/seed"+randInt+".png";
+      message.channel.send(url);
+    }
+
+
+  
+
    }
 };
